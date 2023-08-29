f9624d8896bfd82690e2cdf67011d62574ab7031
zshah-HBKU <72696373+zshah-HBKU@users.noreply.github.com>
2023-08-29 12:48:33 +0300

    Add files via upload

---
+++ Module 6/xypoint.py
@@ -0,0 +1,25 @@
+from math import *
+
+class MyXYPoint:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def set_location(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance_from_origin(self):
+        return sqrt(self.x * self.x + self.y * self.y)
+
+    def distance(self, other):
+        dx = self.x - other.x
+        dy = self.y - other.y
+        return sqrt(dx * dx + dy * dy)
+
+    def __str__(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
